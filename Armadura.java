57c971186aa6cdb6eaf90fd605539d3be2dedae3
Enzo Guimarães <127992419+EnzoGuimaraesBevilaqua@users.noreply.github.com>
2023-09-07 17:52:54 -0300

    Update Armadura.java

--- Armadura.java
+++ Armadura.java
@@ -1,6 +1,16 @@
 package SistemaDeCombate;
 
 public class Armadura {
+  
+    int defesa = 10;
     
-    int defesa;
+    public void setDefesa(int defesa)
+    {
+        this.defesa = defesa;
+    }
+    
+    public int getDefesa()
+    {
+        return this.defesa;
+    }
 }
