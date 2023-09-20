4890c11edc86913fbb4a92f6059695b1e0c19fc4
Antônio Lucas <129812080+Tony38700@users.noreply.github.com>
2023-09-20 07:33:35 -0300

    Update Armadura.java

--- Armadura.java
+++ Armadura.java
@@ -1,16 +1,38 @@
 package SistemaDeCombate;
 
 public class Armadura {
-  
-    int defesa = 10;
     
-    public void setDefesa(int defesa)
+    private String nomeDaArmadura;
+    private double valorDeArmadura;
+    private double constanteDeDefesa;
+    
+    public void setNomeDaArmadura(String nomeDaArmadura)
     {
-        this.defesa = defesa;
+        this.nomeDaArmadura = nomeDaArmadura;
     }
     
-    public int getDefesa()
+    public String getNomeDaArmadura()
     {
-        return this.defesa;
+        return this.nomeDaArmadura;
+    }
+    
+    public void setConstanteDeDefesa(double constanteDeDefesa)
+    {
+        this.constanteDeDefesa = constanteDeDefesa;
+    }
+    
+    public double getConstanteDeDefesa()
+    {
+        return this.constanteDeDefesa;
+    }
+    
+    public void setValorDeArmadura(double valorDeArmadura)
+    {
+        this.valorDeArmadura = valorDeArmadura;
+    }
+    
+    public double getValorDeArmadura()
+    {
+        return this.valorDeArmadura;
     }
 }
